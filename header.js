2180dbeb63b405eb18886b7cf8f868f8c0b2a053
Davi Ramos <rdavi6558@gmail.com>
2025-01-25 03:41:17 -0300

    atualizacoes

--- header.js
+++ header.js
@@ -14,3 +14,28 @@ $(document).click(() => {
 $menu.click((event) => {
     event.stopPropagation();
 });
+
+$(document).ready(() => {
+    $('button.music').click(() => {
+      $('dialog.music')[0].showModal();
+    });
+
+    $('button.playlist').click(() => {
+      $('dialog.playlist')[0].showModal();
+    });
+
+    $('button.categoria').click(() => {
+      $('dialog.categoria')[0].showModal();
+    });
+
+    $('button.cancelar').click(function () {
+      $(this).closest('dialog')[0].close();
+    });
+  });
+
+  let $colorInput = $("#color");
+  let $colorBox = $(".color-box");
+  
+  $colorInput.on("input", () => {
+    let selectedColor = $colorInput.val();
+  });
