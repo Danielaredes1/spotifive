df5641993e98b6f9c95557aaaf76ff31d0bc2510
Davi Ramos <rdavi6558@gmail.com>
2025-01-25 15:09:40 -0300

    ,

--- header.js
+++ header.js
@@ -1,41 +1,38 @@
-
-let $menuButton = $('#mais_icon');
-let $menu = $('.menu');
-
-$menuButton.click(() => {
-    $menu.toggleClass('active');
-    event.stopPropagation();
-});
-
-$(document).click(() => {
-    $menu.removeClass('active');
-});
-
-$menu.click((event) => {
-    event.stopPropagation();
-});
-
 $(document).ready(() => {
-    $('button.music').click(() => {
+  let $menuButton = $('#mais_icon');
+  let $menu = $('.menu');
+
+  $menuButton.click((event) => {
+      $menu.toggleClass('active');
+      event.stopPropagation();
+  });
+
+  $(document).click(() => {
+      $menu.removeClass('active');
+  });
+
+  $menu.click((event) => {
+      event.stopPropagation();
+  });
+
+  $('button.music').click(() => {
       $('dialog.music')[0].showModal();
-    });
+  });
 
-    $('button.playlist').click(() => {
+  $('button.playlist').click(() => {
       $('dialog.playlist')[0].showModal();
-    });
+  });
 
-    $('button.categoria').click(() => {
+  $('button.categoria').click(() => {
       $('dialog.categoria')[0].showModal();
-    });
+  });
 
-    $('button.cancelar').click(function () {
+  $('button.cancelar').click(function () {
       $(this).closest('dialog')[0].close();
-    });
   });
 
   let $colorInput = $("#color");
-  let $colorBox = $(".color-box");
-  
   $colorInput.on("input", () => {
-    let selectedColor = $colorInput.val();
+      let selectedColor = $colorInput.val();
   });
+});
