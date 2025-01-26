928794f2b5b1b153f271400a76156f43a41bbe35
Danielaredes1 <161955423+Danielaredes1@users.noreply.github.com>
2025-01-26 13:00:04 -0300

    Create painel.php

---
+++ painel.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-vindo ao Spotfive!</title>
+    <link rel="stylesheet" href="painel.css">
+</head>
+<body>
+    <header>
+        <div id="logar" class="log">
+            <a href="login.php">Login</a>
+        </div>
+        <div id="cadastrar" class="log">
+            <a href="cadastro.php">Cadastre-se</a>
+        </div>
+    </header>
+    <h1>Bem-vindo ao <br> Spotfive!</h1>
+</body>
+</html>
