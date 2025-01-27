b4d53ad7fe880731ff5224b5a012f4393cc00da8
Samuel Gonçalves <goncalvitoss@gmail.com>
2025-01-27 14:56:20 -0300

    Update sertanejo.php

--- sertanejo.php
+++ sertanejo.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Playlist Sertanejo</title>
-    <link rel="stylesheet" href="sertanejo.css">
+    <link rel="stylesheet" href="musica.css">
 </head>
 <body>
     <header>
