8e4523ce515170793532c866994984552ed3e21b
Samuel Gonçalves <goncalvitoss@gmail.com>
2025-01-27 14:42:37 -0300

    Criei o HTML da Playrist Internacional

---
+++ internacional.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playlist internacional</title>
+    <link rel="stylesheet" href="musica.css">
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <div id="logotipo">
+                <a href="index.php"><img id="logo" src="imgs/logo.png" alt="logotipo"></a>
+                <h3 id="site_name">SPOTFIVE</h3>
+            </div>
+        </div>
+    </header>
+
+    <section class="playlist-section">
+        <h2>TOP internacional - Spotifive</h2>
+
+        <div class="audio-player">
+            <audio id="audio-player" controls>
+                <source id="audio-source" src="" type="audio/mp3">
+            </audio>
+        </div>
+
+ 
+        <div class="music-list">
+            <ul>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe Billie Eilish - WILDFLOWER (Official Lyric Video) [l08Zw-RY__Q].mp3">Wildflower</a>
+                    <span class="music-legend">Billie Eilish</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe Friends [qFdoMMR-7fc].mp3">Friends</a>
+                    <span class="music-legend">Chase Atlantic</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe good 4 u [aeDdS9aIpck].mp3">Good 4u</a>
+                    <span class="music-legend">Olivia Rodrigo</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe Lana Del Rey - Young and Beautiful [o_1aF54DO60].mp3">Young and Beautiful</a>
+                    <span class="music-legend">Lana del Rey</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe Michael Jackson - Billie Jean (Official Video) [Zi_XLOBDo_Y].mp3">Billie Jean</a>
+                    <span class="music-legend">Michael Jackson</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe Olivia Rodrigo - drivers license (Official Video) [ZmDBbnmKpqQ].mp3">Drivers License</a>
+                    <span class="music-legend">Olivia Rodrigo</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe No. 1 Party Anthem [mGUjVbsYG6E].mp3">No. 1 Party Anthem</a>
+                    <span class="music-legend">Artic Monkeys</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe Sade - Like a Tattoo (Audio) [oP4sBeV7llE].mp3">Like a Tatto</a>
+                    <span class="music-legend">Sade</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe Taylor Swift - Shake It Off [nfWlot6h_JM].mp3">Shake it Off</a>
+                    <span class="music-legend">Taylor Swift</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe The Weeknd - After Hours (Audio) [ygTZZpVkmKg].mp3">After Hours</a>
+                    <span class="music-legend">The Weeknd</span>
+                </li>
+                <li>
+                    <a href="#" class="music-item" data-src="audios/Iframe The Neighbourhood - Reflections (Official Audio) [x47TgeRJtH0].mp3">Reflections</a>
+                    <span class="music-legend">The Neighbourhood</span>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2025 Spotfive. Todos os direitos reservados.</p>
+    </footer>
+
+    <script src="musica.js"></script>
+</body>
+</html>
